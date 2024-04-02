4054c59eae12d49932bb096402403f4e37867417
Francis Gallardo <francis.gallardo@globe.com.ph>
2024-04-02 22:15:45 +0800

    Used the poke api to get types.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import Dex from "./Dex";
 
+const BASE_URL = 'https://pokeapi.co/api/v2/type/';
+
 const App = () => {
+  const [types, setTypes] = useState([]);
+
+  useEffect(() => {
+    requestTypes();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function requestTypes() {
+    const res = await fetch(BASE_URL);
+    const json = await res.json();
+  
+    console.log(json);
+
+    setTypes(json.results);
+  }
+
   return (
     <div className="container">
       <h1>Pokedéx Beta</h1>
-      <Dex/>
+      { !types?.length ? ( <p>No Types Found</p> ) : <Dex results={types}/>}
     </div>
   );
 };
--- src/Dex.jsx
+++ src/Dex.jsx
@@ -1,18 +1,35 @@
-const Dex = () => {
+import { useState } from "react";
+
+const Dex = ({ results }) => {
+  const [selectedType1, setType1] = useState("");
+  const [selectedType2, setType2] = useState("");
+
   return (
     <form>
 
       <label htmlFor="type">
         Type 1
-        <select id="type1">
+        <select 
+        id="type1"
+        value={selectedType1}
+        onChange={ e => setType1(e.target.value)}>
           <option />
+          {results.map((result) => (
+            <option key={result.name}>{result.name}</option>
+          ))}
         </select>
       </label>
 
       <label htmlFor="type">
         Type 2
-        <select id="type2">
+        <select 
+        id="type2"
+        value={selectedType2}
+        onChange={ e => setType2(e.target.value)}>
           <option />
+          {results.map((result) => (
+            <option key={result.name}>{result.name}</option>
+          ))}
         </select>
       </label>
 
